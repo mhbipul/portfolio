0493ccb0df3b72e8bf44b9b7aff7a901382e8e64
Bipul <mhbipul99bd@gmail.com>
2025-02-15 03:06:46 +0600

    services

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import About from "./components/About-Me/About";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
+import Service from "./components/Services/Service";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Hero />
       <About />
+      <Service />
     </>
   );
 }
