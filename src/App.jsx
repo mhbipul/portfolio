c4fdc38d79159821622ece7b51a07e8e1a58553d
Bipul <mhbipul99bd@gmail.com>
2025-02-15 01:50:52 +0600

    Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
+import About from "./components/About-Me/About";
+import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Hero />
+      <About />
     </>
   );
 }
